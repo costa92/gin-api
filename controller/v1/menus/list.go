3cb9a00f2951173d2976e49c198bab0594fa8775
costa <costa@hellotalk.com>
2022-10-14 00:08:26 +0800

    fector: 菜单管理

--- controller/v1/menus/list.go
+++ controller/v1/menus/list.go
@@ -2,12 +2,13 @@ package menus
 
 import (
 	"github.com/costa92/errors"
+	"github.com/costa92/go-web/internal/middleware"
 	"github.com/gin-gonic/gin"
+	"time"
 
 	"github.com/costa92/go-web/model"
 	"github.com/costa92/go-web/pkg/code"
 	"github.com/costa92/go-web/pkg/util"
-	"github.com/costa92/go-web/pkg/util/gormutil"
 )
 
 type MenuListRequest struct {
@@ -26,20 +27,45 @@ func (m *MenuController) List(ctx *gin.Context) {
 		util.WriteResponse(ctx, errors.WithCode(code.ErrBind, err.Error()), nil)
 		return
 	}
-	// 分页处理
-	ol := gormutil.Unpointer(&req.Page, &req.PageSize)
 	// 处理查询数据
 	if err := m.MysqlStorage.
-		Model(&model.Menu{}).Scopes(gormutil.Paginate(ol.Offset, ol.Limit)).
-		Order("id desc").
+		Model(&model.Menu{}).
+		Order("menu_sort asc").
 		Find(&ret.Items).
-		Offset(-1).
-		Limit(-1).
-		Count(&ret.TotalCount).
 		Error; err != nil {
 		util.WriteResponse(ctx, errors.WithCode(code.ErrDatabase, err.Error()), "查询数据库错误")
 		return
 	}
-	util.WriteSuccessResponse(ctx, ret)
-	util.WriteSuccessResponse(ctx, "ret")
+	trees := listMenuTree(ret.Items, 0)
+	util.WriteSuccessResponse(ctx, trees)
+
+}
+
+type ListMenuTreeItem struct {
+	*model.Menu
+	FormattedCreatedAt string             `json:"formatted_created_at"`
+	FormattedUpdatedAt string             `json:"formatted_updated_at"`
+	Children           []ListMenuTreeItem `json:"children"`
+}
+
+type GetMenuTreeResponse struct {
+	Items []ListMenuTreeItem `json:"items"`
+}
+
+func listMenuTree(menus []*model.Menu, parentID uint) []ListMenuTreeItem {
+	tree := make([]ListMenuTreeItem, 0)
+	for _, menu := range menus {
+		if parentID != menu.ParentID {
+			continue
+		}
+		item := ListMenuTreeItem{
+			Menu:               menu,
+			FormattedCreatedAt: time.Unix(menu.CreatedAt, 0).Format(middleware.TimeFieldFormat),
+			FormattedUpdatedAt: time.Unix(menu.UpdatedAt, 0).Format(middleware.TimeFieldFormat),
+			Children:           []ListMenuTreeItem{},
+		}
+		item.Children = listMenuTree(menus, menu.ID)
+		tree = append(tree, item)
+	}
+	return tree
 }
